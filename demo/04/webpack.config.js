67469329b757fcc0896efa5a2030a374337b7196
Yang <liu.yang@ztesoft.com>
2018-07-04 15:17:57 +0800

    提交04demo

---
+++ demo/04/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path')
+const fs = require('fs')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackBar = require('webpackbar')
+
+const appDirectory = fs.realpathSync(process.cwd())
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  resolve: {
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx']
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.(js|jsx|mjs)$/,
+        include: resolveApp('src'),
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true
+        }
+      },
+      {
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.less$/,
+            use: [
+              {
+                loader: require.resolve('style-loader')
+              },
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
+              },
+              {
+                loader: require.resolve('less-loader')
+              }
+            ]
+          },
+          {
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: resolveApp('public/index.html')
+    }),
+    new WebpackBar()
+  ]
+}
---
+++ doc/关于React.md
@@ -0,0 +1,107 @@
+## React
+
+React 是一个用于构建用户界面的 JAVASCRIPT 库
+React 主要用于构建 UI，很多人认为 React 是 MVC 中的 V（视图）
+
+### JSX
+
+JSX 是一个看起来很像 XML 的 JavaScript 语法扩展。
+
+```javascript
+render () {
+  const name = 'React'
+  return (
+    <div className="demo">
+      Hello {name} !
+    </div>
+  )
+}
+```
+
+### 组件
+
+一些 HTML、css、js 的集合
+
+```javascript
+const HelloMessage = () => {
+  const name = "React";
+  return <div className="demo">Hello {name} !</div>;
+};
+
+const Demo = () => {
+  return (
+    <div>
+      <HelloMessage />
+    </div>
+  );
+};
+```
+
+### 有状态
+
+有自己的生命周期、状态
+
+```javascript
+import React, { Component } from "react";
+
+class Demo extends Component {
+  state = {
+    name: "React"
+  };
+  componentDidMount() {
+    // ...
+  }
+  render() {
+    return <div>Hello {this.state.name}</div>;
+  }
+}
+```
+
+### 无状态组件
+
+没有自己的生命周期、状态，只有属性
+
+```javascript
+import React from "react";
+
+const Demo = props => {
+  return <div>Hello {props.name} !</div>;
+};
+```
+
+### 状态
+
+有状态组件中定义的`state`
+
+### 属性
+
+ 上一层传递下来的`props`
+
+### 方法
+
+```javascript
+import React, { Component } from "react";
+
+class Demo extends Component {
+  state = {
+    name: "React"
+  };
+  clicl = () => {
+    this.setState({
+      name: "React 666"
+    });
+  };
+  render() {
+    return (
+      <div>
+        <p>Hello {this.state.name}</p>
+        <button onClick={this.click}>click</button>
+      </div>
+    );
+  }
+}
+```
+
+### 单向数据流
+
+React 中的数据永远是从上层到下层的
