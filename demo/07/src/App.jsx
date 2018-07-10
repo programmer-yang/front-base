a6d4b16aec5cc7566de1e7f3f968e6e69bf7a2f6
Yang <liu.yang@ztesoft.com>
2018-07-10 17:01:55 +0800

    添加antd支持

---
+++ demo/07/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const App = () => {
+  return (
+    <div>
+      <Button type="primary">Click</Button>
+    </div>
+  )
+}
+
+export default App
